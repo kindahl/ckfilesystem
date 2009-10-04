f225cff7071e10ccfcb3cc2e0bdaad547db58533
Christian Kindahl <christian@kindahl.com>
2009-10-04 20:23:20 +0000

    Updated reconfigure and autoconf scripts.

--- src/iso9660pathtable.cc
+++ src/iso9660pathtable.cc
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
 #include <algorithm>
 #include "ckfilesystem/iso9660pathtable.hh"
 #include "ckfilesystem/stringtable.hh"
